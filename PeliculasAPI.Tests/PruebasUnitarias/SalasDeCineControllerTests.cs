d69b73a8be45281ef0620aa034faaefcdc891475
Angelo Gonzalez <angelo.gonzalez@live.com>
2022-06-16 12:34:53 -0400

    Controlador de Cines - Pruebas Unitarias

---
+++ PeliculasAPI.Tests/PruebasUnitarias/SalasDeCineControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using NetTopologySuite;
+using NetTopologySuite.Geometries;
+using PeliculasAPI.Controllers;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entidades;
+
+namespace PeliculasAPI.Tests.PruebasUnitarias
+{
+    [TestClass]
+    public class SalasDeCineControllerTests : BasePruebas
+    {
+        [TestMethod]
+        public async Task ObtenerSalasDeCineA5kilometrosOMenos()
+        {
+            var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+
+            using (var context = LocalDbDatabaseInitializer.GetDbContextLocalDb(false))
+            {
+                var salasDeCine = new List<SalaDeCine>()
+                {
+                    new SalaDeCine{ Nombre = "Plazuela Independencia", Ubicacion = geometryFactory.CreatePoint(new Coordinate(-70.578375, -33.601335)) }
+                };
+
+                context.AddRange(salasDeCine);
+                await context.SaveChangesAsync();
+            }
+
+            var filtro = new SalaDeCineCercanoFiltroDTO()
+            {
+                DistanciaEnKms = 1,
+                Latitud = -70.590201,
+                Longitud = -33.599142
+            };
+
+            using (var context = LocalDbDatabaseInitializer.GetDbContextLocalDb(false))
+            {
+                var mapper = ConfigurarAutoMapper();
+                var controller = new SalasDeCineController(context, mapper, geometryFactory);
+                var respuesta = await controller.Cercanos(filtro);
+                var valor = respuesta.Value;
+                Assert.AreEqual(0, valor?.Count);
+            }
+
+        }
+    }
+}
